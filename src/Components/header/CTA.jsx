b747e1d9c2b0940c6d4f4d5457e0fafa66e953ca
Safi <safisiddiquim@gmail.com>
2022-03-16 10:38:53 -0500

    updated resume

--- src/Components/header/CTA.jsx
+++ src/Components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Resume from "../../assets/Resume.pdf";
+import Resume from "../../assets/ResumeFinal.pdf";
 const CTA = () => {
   return (
     <div className="cta">
